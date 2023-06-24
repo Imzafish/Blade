6e3da343d9ab067cecc4d9ae1859bb9d2e39f413
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-06-24 17:36:03 +1000

    Update Blade.py

--- Blade.py
+++ Blade.py
@@ -1,21 +1,22 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.messages = True  # Enable message content intent
+
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 bounties = {}  # Dictionary to store bounties
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.command()
+@bot.command(description='Accept a bounty')
 async def bounty(ctx, name: str):
     # Handle bounty command here
     bounties[name] = True
     await ctx.send(f'Bounty accepted for {name}!')
 
-@bot.command()
+@bot.command(description='Perform a task')
 @commands.has_role('Blade')  # Only users with the role "Blade" can execute this command
 async def task(ctx, name: str):
     # Perform the task here
@@ -25,12 +26,7 @@ async def task(ctx, name: str):
     else:
         await ctx.send(f'{name} does not have an active bounty.')
 
-@task.error
-async def task_error(ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        await ctx.send("You don't have the required role to execute this command.")
-
-@bot.command()
+@bot.command(description='List active bounties')
 @commands.has_role('Blade')  # Only users with the role "Blade" can execute this command
 async def bountylist(ctx):
     active_bounties = [name for name, status in bounties.items() if status]
@@ -39,10 +35,8 @@ async def bountylist(ctx):
     else:
         await ctx.send('No active bounties.')
 
-@bountylist.error
-async def bountylist_error(ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        await ctx.send("You don't have the required role to execute this command.")
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
 
-bot.run('YOUR_BOT_TOKEN')
-
+bot.run('MTA5NjQwNzQ4ODAxNTg5MjQ5MA.GYWdl5.SNshzvb9vy3Q9jQrD-_CiDHNQEAde1z2RkrwYY')
